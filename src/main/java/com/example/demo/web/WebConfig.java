4c3b4096692dd5fc6d498c88ce07b680bf1b1fe3
Iva Dimitrijevic <ivaivaiva.id@gmail.com>
2023-06-18 13:08:24 +0200

    Izmene oko weba

--- src/main/java/com/example/demo/web/WebConfig.java
+++ src/main/java/com/example/demo/web/WebConfig.java
@@ -11,7 +11,35 @@ package com.example.demo.web;
 //import org.springframework.security.web.header.writers.StaticHeadersWriter;
 //import org.springframework.web.servlet.config.annotation.CorsRegistry;
 //import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.web.header.writers.StaticHeadersWriter;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+//import jakarta.servlet.*;
+//import jakarta.servlet.http.*;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.web.header.writers.StaticHeadersWriter;
+//import java.io.IOException;
 
 /**
  *
@@ -20,20 +48,20 @@ package com.example.demo.web;
 //@SuppressWarnings("deprecation")
 //@Configuration
 //@EnableWebMvc
-public class WebConfig{} //extends WebMvcConfigurerAdapter{
-//    
+//public class WebConfig implements WebMvcConfigurer{
+//   
 //    @Override
 //	public void addCorsMappings(CorsRegistry registry) {
 //		registry.addMapping("/**").allowedOrigins("*");
 //	}
-//	 @Override
-//	    public void addCorsMappings(CorsRegistry registry) {
-//	        registry.addMapping("/**")
-//	                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//	                .allowedOrigins("*")
-//	                .allowedHeaders("Content_Type", "Authorization");
-//	    }
-
+////	 @Override
+////	    public void addCorsMappings(CorsRegistry registry) {
+////	        registry.addMapping("/**")
+////	                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+////	                .allowedOrigins("*")
+////	                .allowedHeaders("Content_Type", "Authorization");
+////	    }
+//
 //	@Bean
 //	public WebSecurityConfigurerAdapter webSecurity() {
 //		return new WebSecurityConfigurerAdapter() {
@@ -45,6 +73,166 @@ public class WebConfig{} //extends WebMvcConfigurerAdapter{
 //				http.headers().addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "*"));
 //			}
 //		};
-//return null;
 //	}
+//        @Bean
+//    public WebSecurityConfigurerAdapter webSecurityConfigurerAdapter() {
+//        return new WebSecurityConfigurerAdapter() {
+//            @Override
+//            protected void configure(HttpSecurity http) throws Exception {
+//                http.cors().and().csrf().disable();
+//                http.headers().addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "*"));
+//            }
+//        };
+//    }
 //}
+
+//@SuppressWarnings("deprecation")
+//@Configuration
+//@EnableWebMvc
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**").allowedOrigins("*");
+//    }
+//
+//    @Bean
+//    public WebSecurityConfigurerAdapter webSecurityConfigurerAdapter() {
+//        return new WebSecurityConfigurerAdapter() {
+//
+//            @Override
+//            protected void configure(HttpSecurity http) throws Exception {
+//                http.cors().and().csrf().disable();
+//                http.cors();
+//                http.headers().addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "*"));
+//            }
+//        };
+//    }
+//}
+//@SuppressWarnings("deprecation")
+//@Configuration
+////@EnableWebMvc
+//public class WebConfig implements JakartaConfig {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**").allowedOrigins("*");
+//    }
+////   @Override
+////    public void addCorsMappings(CorsRegistry registry) {
+////        registry.addMapping("/**")
+////                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+////                .allowedOrigins("*")
+////                .allowedHeaders("Content_Type", "Authorization");
+////    }
+//
+//    @Bean
+//    public SecurityFilter webSecurity() {
+//        return new SecurityFilter();
+//    }
+//
+//    public static class SecurityFilter implements Filter {
+//
+//        @Override
+//        public void init(FilterConfig filterConfig) throws ServletException {
+//            // Optional: You can perform any initialization logic here
+//        }
+//
+//        @Override
+//        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//                throws IOException, ServletException {
+//            HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+//            httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+//            chain.doFilter(request, response);
+//        }
+//
+//        @Override
+//        public void destroy() {
+//            // Optional: You can perform any cleanup logic here
+//        }
+//    }
+//}
+
+
+
+
+
+//import jakarta.servlet.*;
+//import jakarta.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//
+//@SuppressWarnings("deprecation")
+//@Configuration
+//public class WebConfig {
+//
+//    @Bean
+//    public FilterRegistrationBean<SecurityFilter> securityFilter() {
+//        FilterRegistrationBean<SecurityFilter> registrationBean = new FilterRegistrationBean<>();
+//        registrationBean.setFilter(new SecurityFilter());
+//        registrationBean.addUrlPatterns("/*");
+//        return registrationBean;
+//    }
+//
+//    public static class SecurityFilter implements Filter {
+//
+//        @Override
+//        public void init(FilterConfig filterConfig) throws ServletException {
+//            // Optional: You can perform any initialization logic here
+//        }
+//
+//        @Override
+//        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//                throws IOException, ServletException {
+//            HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+//            httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+//            chain.doFilter(request, response);
+//        }
+//
+//        @Override
+//        public void destroy() {
+//            // Optional: You can perform any cleanup logic here
+//        }
+//    }
+//}
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*");
+	}
+//	 @Override
+//	    public void addCorsMappings(CorsRegistry registry) {
+//	        registry.addMapping("/**")
+//	                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//	                .allowedOrigins("*")
+//	                .allowedHeaders("Content_Type", "Authorization");
+//	    }
+
+	@Bean
+	public WebSecurityConfigurerAdapter webSecurity() {
+		return new WebSecurityConfigurerAdapter() {
+
+			@Override
+			protected void configure(HttpSecurity http) throws Exception {
+				http.cors().and().csrf().disable();
+				http.cors();
+				http.headers().addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "*"));
+			}
+		};
+	}
+}
